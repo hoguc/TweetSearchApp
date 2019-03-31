da2169b2a0713e47a16d680c749128604f6ade2d
hoguc <uku.h1r8@gmail.com>
2019-03-31 12:04:16 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'likes/create'
-  get 'likes/destroy'
   root 'home#index'
   get 'home/index', to: 'home#index', as: 'index_home'
   get 'tweets/search', to: 'tweets#search', as: 'search_tweet'
   get 'tweets/top', to: 'tweets#top', as: 'top_tweets'
+  get 'likes/create'
+  get 'likes/destroy'
   get 'likes/index', to: 'likes#index', as: 'index_like'
   post 'likes/create', to: 'likes#create', as: 'create_likes'
   post 'likes/destroy', to: 'likes#destroy', as: 'destroy_likes'
